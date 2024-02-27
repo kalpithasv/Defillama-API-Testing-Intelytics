671fa38d27a93529ca99da654a0c7805081eb8ad
Pranav2456 <pranavvinodan@gmail.com>
2024-02-27 23:54:45 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { set } from 'mongoose';
 
 /* DeFi Llama API Endpoints
  Protocol Endpoints: https://api.llama.fi/protocols
@@ -32,6 +33,7 @@ const DefiStats = () => {
   const [stats, setStats] = useState({ tvl: null, change: null });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [helixTvlData, setHelixTvlData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,7 +43,13 @@ const DefiStats = () => {
         const response2 = await axios.get("https://api.llama.fi/summary/dexs/astroport?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyVolume");
         const response3 = await axios.get("https://api.llama.fi/summary/dexs/helix?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyVolume");
         const response4 = await axios.get("https://api.llama.fi/v2/chains");
-        const response5 = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+        // const response5 = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+        const response6 = await axios.get("https://coins.llama.fi/prices/current/ethereum:0xe28b3b32b6c345a34ff64674606124dd5aceca30,secret:secret14706vxakdzkz9a36872cs62vpl5qd84kpwvpew,binance-smart-chain:0xa2b726b1145a4773f68593cf171187d8ebe4d495,cosmos:ibc%2F64BA6E31FE887D66C6F8F31C7B1A80C7CA179239677B4088BB55F5EA07DBE273?searchWidth=4h");
+        const response7 = await axios.get("https://api.llama.fi/protocol/helix");
+       
+    const price = response6.data.coins["ethereum:0xe28b3b32b6c345a34ff64674606124dd5aceca30"].price;
+    console.log(price);
+
         // Volume API Testing.
         const astroportVolume = response2.data;
         const astroport24hVolume = astroportVolume.total24h;
@@ -57,13 +65,20 @@ const DefiStats = () => {
         console.log(helix24hVolume);
         console.log(helix7dVolume);
 
+        // Date specific TVL (Helix)
+        const helixData = response7.data;
+        setHelixTvlData(helixData.chainTvls.Injective.tvl);
+        const tvlValues = helixTvlData.map(({ date, totalLiquidityUSD }) => ({ date, totalLiquidityUSD }));
+        console.log(tvlValues);
         // Coingecko API 
+        /*
         const coingeckoAPI = response5.data;
         const injectiveCoin = coingeckoAPI.find((coin) => coin.id === "injective-protocol");
         const injectivePrice = injectiveCoin.current_price;
         const injectiveMarketCap = injectiveCoin.market_cap;
         console.log(injectivePrice);
         console.log(injectiveMarketCap);
+        */
 
         // Injective TVL
 
@@ -93,6 +108,8 @@ const DefiStats = () => {
           // DojoSwap Staking Value
           const dojoswapStaking = dojoswap.staking;
           console.log(dojoswapStaking);
+
+          
         setStats({ tvl: totalTvl, change: oneDayChange });
       } catch (error) {
         setError(error);
@@ -100,6 +117,8 @@ const DefiStats = () => {
         setIsLoading(false);
       }
     };
+
+    
     fetchData();
   }, []);
 
